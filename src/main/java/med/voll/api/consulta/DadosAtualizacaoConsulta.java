310ad92325236944fa43cef22bc2a23e41b44f06
Abboud21 <abboudmucy@gmail.com>
2025-09-22 11:40:14 -0300

    Consulta

---
+++ src/main/java/med/voll/api/consulta/DadosAtualizacaoConsulta.java
@@ -0,0 +1,18 @@
+package med.voll.api.consulta;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+
+public record DadosAtualizacaoConsulta(
+       @NotNull
+        Long id,
+        @NotBlank
+        String nome,
+        @NotBlank
+        String telefone,
+        @NotBlank
+        String email,
+        Boolean ativo
+) {
+} 
